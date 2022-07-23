0600ef56e906f5c810e1fde68d31944f7ce6938e
amig05c <amitghosh0595c@gmail.com>
2022-07-23 11:33:30 +0530

    carouselUpdate

--- src/Pages/women/WomenCarousel .jsx
+++ src/Pages/women/WomenCarousel .jsx
@@ -1,20 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box,Image } from '@chakra-ui/react'
 
-const WomenCarousel  = () => {
-  
 
+const imgUrl= ['https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/MAX-Banner1-desktop-Men-19JULY2022.png',
+'https://lmsin.net/cdn-cgi/image/w=1232,q=85,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/MAX-Banner7-desktop-CommonB2G2-17JUNE2022.gif',
+
+'https://lmsin.net/cdn-cgi/image/w=1232,q=60,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/MAX-Banner5-desktop-Men-18JULY2022.png'
+]
+const WomenCarousel  = () => {
+  const [url,setUrl] = useState('')
+
+  let i=0
     useEffect(()=>{
-        setInterval(()=>{
-            // console.log(`amit`)
-            // return <div> <img src="" alt="" /></div>
-        },3000)
-    },[])
+   let  id=  setInterval(()=>{
+          if(i==imgUrl.length){
+            i=0
+          }
+            setUrl(imgUrl[i])
+            i++
+          },4000)
+         return ()=>  clearInterval(id)
+    },[i])
   return (
     <Box bg='' w='100%' p={4} color='white'  padding='0'>
-    <Image src="/womenImg/MAX-Banner1-desktop-Women-14JULY2022.png" w={'100%'} />
+    <Image src={url} w={'100%'} />
     </Box>
   )
 }
 
 export default WomenCarousel 
+
+// /womenImg/MAX-Banner1-desktop-Women-14JULY2022.png
